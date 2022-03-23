4801d17675ca94f6881867aa55251d6e00a34bf8
Michael Baklykov <baklykovmichael@gmail.com>
2022-03-23 21:33:04 +0600

    [+] 06-homework pom changes

--- src/main/java/it/sevenbits/web/application/Application.java
+++ src/main/java/it/sevenbits/web/application/Application.java
@@ -3,9 +3,16 @@ package it.sevenbits.web.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    /**
+     * main
+     * @param args - String[]
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
