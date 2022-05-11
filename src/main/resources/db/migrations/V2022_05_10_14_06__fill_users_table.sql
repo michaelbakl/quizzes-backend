dd1d4556d7189c8c846d125b74e774f78a1225e0
Michael Baklykov <baklykovmichael@gmail.com>
2022-05-11 18:56:24 +0600

    [*] 09-homework passwords for user and admin changed

--- src/main/resources/db/migrations/V2022_05_10_14_06__fill_users_table.sql
+++ src/main/resources/db/migrations/V2022_05_10_14_06__fill_users_table.sql
@@ -1,6 +1,6 @@
 insert into users values
 ('7df2eb5c-d119-11ec-9d64-0242ac120002', 'user@example.com',
- '$2a$12$UWtdPoXWalzaxxA4NzqPau6gqxvU.YKZtDV0rMzXc/2d0BqGqN9r2', true),
+ '$2a$12$0RVBgU/tVIsbOBW5VFOCwurhp/wEIZ05lMUbp/jSApz0zpJulqYOa', true),
 ('7df2ed46-d119-11ec-9d64-0242ac120002', 'admin@example.com',
- '$2a$12$6PeOIvwahyBiSp64vuGoqO4hfbRUG41/bcz.XJzc7zNVogIucsGY6', true)
+ '$2a$12$hwbFyQIB/6NbeAMPmG0Kg.MRbMl0AGu3ehd7bU2uL23vVRzOLAtzW', true)
                   on conflict do nothing;
--- src/main/resources/db/migrations/V2022_05_10_14_10__fill_user_roles_table.sql
+++ src/main/resources/db/migrations/V2022_05_10_14_10__fill_user_roles_table.sql
@@ -1,3 +1,2 @@
 insert into userroles values ('7df2eb5c-d119-11ec-9d64-0242ac120002', 'USER'),
-                             ('7df2ed46-d119-11ec-9d64-0242ac120002', 'USER'),
                              ('7df2ed46-d119-11ec-9d64-0242ac120002', 'ADMIN') on conflict do nothing;
